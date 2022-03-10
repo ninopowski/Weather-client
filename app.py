a499de6b363ca1e438489a60176cde92803bdc57
ninopowski <ninoskbd@gmail.com>
2022-03-10 11:15:32 +0100

    With named tuples

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 #LWSK
 import requests
+import bs4
+import collections
+
+WeatherInfo = collections.namedtuple("WeatherInfo",
+                                     "condition, temperature")
 
 def main():
     # print the header
@@ -12,8 +17,10 @@ def main():
     html = get_html_from_web(post_code)
 
     # parse the html
-    # display the forecast
+    report = get_weather_from_html(html)
 
+    # display the forecast
+    show_weather(report.condition, report.temperature)
 
 def print_header():
     print("---------------------")
@@ -27,6 +34,27 @@ def get_html_from_web(post_code):
     response = requests.get(url)
     #print(response.status_code)
     return response.text
+
+
+def get_weather_from_html(html):
+    location_css = ""
+    weather_condition_css = "div.condition-icon p"
+    temperature_css = "span.wu_value"
+
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    condition = soup.find(name="div", class_="condition-icon").get_text()
+    temperature = soup.find(name="span", class_="temp").get_text()
+    report = WeatherInfo(condition=condition, temperature=temperature)
+    return report
+
+def show_weather(cond, temp):
+    print(f"Today at LWSK the conditions are {cond} and it feels like {temp}")
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
